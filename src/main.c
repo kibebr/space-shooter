1616c268b98fb638b98051985a7ab56109813860
kibe <47016873+kibebr@users.noreply.github.com>
2019-08-31 03:16:26 -0400

    Added all the new changes to the game

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "player.h"
 #include "gamestates.h"
+#include "gui.h"
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -9,12 +10,27 @@
 #include <SDL2/SDL_ttf.h>
 
 GameProperties gameProperties;
+const Uint8 *keyboard_state;
 
 // forward declarations
 void render(void);
 void check_inputs(void);
+void update_camera(void);
 
-SDL_Rect stars[STARS];
+typedef struct{
+    float x, y;
+    SDL_Rect starObject;
+} Stars;
+static Stars stars[STARS_QUANTITY];
+
+typedef enum {LEFT_WALL=0, TOP_WALL=1, 
+                           BOTTOM_WALL=2, RIGHT_WALL=3, 
+                                          BORDERS_QUANITY=4} BORDERS;
+
+
+SDL_Rect borders[BORDERS_QUANITY];
+
+SDL_Rect GameCamera;
 
 int main(void){
     CURRENT_GAME_STATE = MAIN_MENU;
@@ -39,16 +55,46 @@ int main(void){
     // allocates enough space for an event handler (game-wide)
     gameProperties.eventHandler = malloc(sizeof(SDL_Event));
     
-    // initializes those pretty little stars
-    for(int i = 0; i < STARS; i++){
-        stars[i].x = rand()%SCREEN_WIDTH;
-        stars[i].y = rand()%SCREEN_HEIGHT;
-        stars[i].w = STARS_WIDTH;
-        stars[i].h = STARS_HEIGHT;
+
+    // initializes the map's walls
+
+    for(int thisWall = 0; thisWall < BORDERS_QUANITY; thisWall++)
+    {
+        if(thisWall == LEFT_WALL || thisWall == RIGHT_WALL)
+        {
+            borders[thisWall].y = 0;
+            borders[thisWall].h = LEVEL_HEIGHT;
+            borders[thisWall].w = 3;
+        }
+        else // (thisWall == TOP_WALL or thisWall == BOTTOM_WALL)
+        {
+            borders[thisWall].w = LEVEL_WIDTH;
+            borders[thisWall].h = 3;
+        }
     }
-    
+
+    // initializes those pretty little stars
+    for(int i = 0; i < STARS_QUANTITY; i++){
+        stars[i].x = rand()%LEVEL_WIDTH;
+        stars[i].y = (rand()%LEVEL_HEIGHT);
+        stars[i].starObject.w = STARS_WIDTH;
+        stars[i].starObject.h = STARS_HEIGHT;
+    }
+
+    // initializes keyboard listener
+    keyboard_state = SDL_GetKeyboardState(NULL);
+
     // initializes player
     init_player();
+
+    // initializes GUI
+    init_GUI();
+
+    // initializes the game camera
+    GameCamera.x = (LEVEL_WIDTH - SCREEN_WIDTH) / 2;
+    GameCamera.y = (LEVEL_HEIGHT - SCREEN_HEIGHT) / 2;
+    GameCamera.w = SCREEN_WIDTH;
+    GameCamera.h = SCREEN_HEIGHT;
 
     // initializes the game states (main menu, etc.)
     init_game_states();
@@ -75,11 +121,10 @@ void check_inputs(void)
     // poll events and checks if user quits the game
     while(SDL_PollEvent(gameProperties.eventHandler))
     {
-
         if(gameProperties.eventHandler->type == SDL_QUIT)
             gameProperties.isRunning = false;
             
-        if(gameProperties.isPlaying)
+        if(getCurrentGameState() == RUNNING)
             player_check_inputs(gameProperties.eventHandler);
     }
 }
@@ -93,28 +138,73 @@ void render(void){
     // renders and updates the current game state
     updateCurrentGameState();
 
+    // applies camera offset to the walls and renders them
+    borders[LEFT_WALL].x = 0 - GameCamera.x;
+    borders[RIGHT_WALL].x = 1276 - GameCamera.x;
+
+    borders[TOP_WALL].y = 0 - GameCamera.y;
+    borders[BOTTOM_WALL].y = 957 - GameCamera.y;
+
+    SDL_SetRenderDrawColor(gameProperties.renderer, 255, 0, 0, 100);
+    for(int thisWall = 0; thisWall < BORDERS_QUANITY; thisWall++)
+        SDL_RenderFillRect(gameProperties.renderer, &borders[thisWall]);
+
     // renders the stars
     SDL_SetRenderDrawBlendMode(gameProperties.renderer, SDL_BLENDMODE_BLEND);
-    for(int i = 0; i < STARS; i++){
+
+    for(int i = 0; i < STARS_QUANTITY; i++)
+    {
         SDL_SetRenderDrawColor(gameProperties.renderer, 255, 255, 255, rand()%105+150);
-        SDL_RenderFillRect(gameProperties.renderer, &stars[i]);
-        stars[i].y++;
+        SDL_RenderFillRect(gameProperties.renderer, &stars[i].starObject);
+
+        if(!playerHasEntered())
+            stars[i].y += 5;
+        else{
+            stars[i].y += 0.5;
+        }
         
-        if(stars[i].y > SCREEN_HEIGHT){
-            stars[i].x = rand()%SCREEN_WIDTH; // randomize their position again
+        // if the stars reach the end of the map, set them back up again and randomize their position
+        if(stars[i].y > LEVEL_HEIGHT){
+            stars[i].x = rand()%LEVEL_WIDTH; 
             stars[i].y = 0;
         }
 
+        // applies camera offset to the stars
+        stars[i].starObject.x = stars[i].x - GameCamera.x;
+        stars[i].starObject.y = stars[i].y - GameCamera.y;
     }
-    
-    // renders the player if the game is in RUNNING state
-    if(gameProperties.isPlaying)
+
+    // do the following ONLY if the game is currently being played
+    if(getCurrentGameState() == RUNNING) 
+    {
         render_player(gameProperties.renderer);
+        render_GUI();
+        if(playerHasEntered())
+            update_camera();
+    }
     
     // renders... everything
     SDL_RenderPresent(gameProperties.renderer);
 }
 
+
+void update_camera(){
+
+    // centers the game camera on the player
+    GameCamera.x = (int)(getPlayerPos('X', false) + PLAYER_WIDTH / 2) - SCREEN_WIDTH / 2;
+    GameCamera.y = (int)(getPlayerPos('Y', false) + PLAYER_HEIGHT / 2) - SCREEN_HEIGHT / 2;
+
+    // if the camera is out of bounds (LEVEL_WIDTH AND LEVEL_HEIGHT NOT SCREEN), stop centering the camera
+    if(GameCamera.x < 0)
+        GameCamera.x = 0;
+    if(GameCamera.y < 0)
+        GameCamera.y = 0;
+    if(GameCamera.x > (LEVEL_WIDTH - GameCamera.w))
+        GameCamera.x = LEVEL_WIDTH - GameCamera.w;
+    if(GameCamera.y > (LEVEL_HEIGHT - GameCamera.h))
+        GameCamera.y = LEVEL_HEIGHT - GameCamera.h;
+
+}
 
 // GETTERS
 
@@ -122,6 +212,27 @@ SDL_Renderer* getGameRenderer(void){
     return gameProperties.renderer;
 }
 
+SDL_Event* getGameEventHandler(void){
+    return gameProperties.eventHandler;
+}
+
+SDL_Rect* getGameCamera(){
+    return &GameCamera;
+}
+
+const Uint8* getGameKeyState(void){
+    return keyboard_state;
+}
+
+// SETTERS
+
+void setIsGamePlaying(bool playing){
+    gameProperties.isPlaying = playing;
+}
+
+void setIsGameRunning(bool running){
+    gameProperties.isRunning = running;
+}
 
 /*
     GAME UTILITIES
@@ -137,7 +248,7 @@ SDL_Renderer* getGameRenderer(void){
 SDL_Texture* LoadTexture(char filePath[]){
    SDL_Surface* surface = IMG_Load(filePath);
    if(!surface){
-       printf("fatal error: not possible to load image\n");
+       printf("fatal error: could not load image\n");
        printf("image path: %s\n", filePath);
        gameProperties.isRunning = false;
    }
@@ -158,4 +269,8 @@ TTF_Font* LoadFont(char filePath[], int fontSize)
     }
     
     return tmp;
+}
+
+double deg2rad(double degrees){
+    return (degrees * PI) / 180;
 }
--- src/main.h
+++ src/main.h
@@ -7,10 +7,14 @@
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
+#define LEVEL_WIDTH 1280
+#define LEVEL_HEIGHT 960
 
-#define STARS 200
+#define STARS_QUANTITY 500
 #define STARS_WIDTH 2
 #define STARS_HEIGHT 2
+
+#define PI 3.141592653589793238462643383279502884
 
 typedef struct {
     bool isRunning;
@@ -19,12 +23,19 @@ typedef struct {
     SDL_Event* eventHandler;
 } GameProperties;
 
-
 // GETTERS
 SDL_Renderer* getGameRenderer(void);
+SDL_Event* getGameEventHandler(void);
+SDL_Rect* getGameCamera(void);
+const Uint8* getGameKeyState(void);
+
+// SETTERS
+void setIsGamePlaying(bool playing);
+void setIsGameRunning(bool running);
 
 // GAME UTILITIES
 SDL_Texture* LoadTexture(char filePath[]);
 TTF_Font* LoadFont(char filePath[], int fontSize);
+double deg2rad(double degrees); // converts degrees to radians
 
 #endif
